5f38f62d7ea00750066b30bd716e31064e97d42a
Scott Lilly <scottlilly@scottlilly.com>
2017-06-01 20:26:18 -0500

    Initial upload

---
+++ Engine/Services/WeatherReader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+using Engine.Models;
+using Engine.Utilities;
+
+namespace Engine.Services
+{
+    public static class WeatherReader
+    {
+        private const string OWM_CURRENT_WEATHER_URL =
+            @"http://api.openweathermap.org/data/2.5/weather?zip={0},{1}&appid={2}&mode=xml";
+
+        private const string OWM_FORECAST_WEATHER_URL =
+            @"http://api.openweathermap.org/data/2.5/forecast?zip={0},{1}&appid={2}&mode=xml";
+
+        private static DateTime _lastTimeRead;
+        private static WeatherForecast _lastForecast;
+
+        public static WeatherForecast GetCurrentForecast(
+            string zipCode, string countryCode, TemperatureUnit unit)
+        {
+            if(_lastForecast != null)
+            {
+                TimeSpan span = DateTime.Now - _lastTimeRead;
+
+                if(span.TotalMinutes < 5)
+                {
+                    return _lastForecast;
+                }
+            }
+
+            string url = OWM_CURRENT_WEATHER_URL
+                .Replace("{0}", zipCode)
+                .Replace("{1}", countryCode)
+                .Replace("{2}", Application.Default.OWMAPIKey);
+
+            using(WebClient client = new WebClient())
+            {
+                XmlDocument weatherXML = new XmlDocument();
+                weatherXML.LoadXml(client.DownloadString(url));
+
+                decimal currentTemperatureKelvin = weatherXML.AttributeAsDecimal("/current/temperature/@value");
+                decimal minTemperatureKelvin = weatherXML.AttributeAsDecimal("/current/temperature/@min");
+                decimal maxTemperatureKelvin = weatherXML.AttributeAsDecimal("/current/temperature/@max");
+
+                WeatherForecast currentWeatherForecast =
+                    new WeatherForecast
+                    {
+                        TemperatureCurrent =
+                            unit == TemperatureUnit.Farenheit
+                                ? KelvinToFarenheit(currentTemperatureKelvin)
+                                : KelvinToCelsius(currentTemperatureKelvin),
+                        TemperatureMinimum =
+                            unit == TemperatureUnit.Farenheit
+                                ? KelvinToFarenheit(minTemperatureKelvin)
+                                : KelvinToCelsius(minTemperatureKelvin),
+                        TemperatureMaximum =
+                            unit == TemperatureUnit.Farenheit
+                                ? KelvinToFarenheit(maxTemperatureKelvin)
+                                : KelvinToCelsius(maxTemperatureKelvin)
+                    };
+
+                _lastTimeRead = DateTime.Now;
+                _lastForecast = currentWeatherForecast;
+
+                return currentWeatherForecast;
+            }
+        }
+
+        private static decimal KelvinToFarenheit(decimal kelvinTemperature)
+        {
+            return (kelvinTemperature - 273M) * 9M / 5M + 32M;
+        }
+
+        private static decimal KelvinToCelsius(decimal kelvinTemperature)
+        {
+            return kelvinTemperature - 273M;
+        }
+    }
+}
---
+++ Valet/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Windows;
+using Engine;
+using Engine.Models;
+using Engine.Services;
+using Engine.Utilities;
+
+namespace Valet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly SpeechSynthesizer _speechSynthesizer = new SpeechSynthesizer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeVoice();
+        }
+
+        private void InitializeVoice()
+        {
+            ReadOnlyCollection<InstalledVoice> voices =
+                _speechSynthesizer.GetInstalledVoices();
+
+            if(!string.IsNullOrWhiteSpace(Settings.Default.VoiceName) &&
+               voices.Any(v => v.VoiceInfo.Name == Settings.Default.VoiceName))
+            {
+                _speechSynthesizer.SelectVoice(Settings.Default.VoiceName);
+            }
+            else
+            {
+                if(Settings.Default.VoiceGender.Equals("Male",
+                                                       StringComparison.CurrentCultureIgnoreCase))
+                {
+                    _speechSynthesizer.SelectVoiceByHints(VoiceGender.Male);
+                }
+                else if(Settings.Default.VoiceGender.Equals("Female",
+                                                            StringComparison.CurrentCultureIgnoreCase))
+                {
+                    _speechSynthesizer.SelectVoiceByHints(VoiceGender.Female);
+                }
+                else
+                {
+                    _speechSynthesizer.SelectVoiceByHints(VoiceGender.Neutral);
+                }
+            }
+
+            _speechSynthesizer.Rate = Settings.Default.VoiceRate;
+            _speechSynthesizer.Volume = Settings.Default.VoiceVolume;
+        }
+
+        private void Speak_OnClick(object sender, RoutedEventArgs e)
+        {
+            WeatherForecast weatherForecast =
+                WeatherReader.GetCurrentForecast(Settings.Default.PostalCode,
+                                                 Settings.Default.CountryCode,
+                                                 (TemperatureUnit)Enum.Parse(typeof(TemperatureUnit),
+                                                                             Settings.Default.TemperatureUnit));
+
+            _speechSynthesizer.Speak(GreetingBuilder.GetCurrentGreetingFor(Settings.Default.UserName));
+
+            _speechSynthesizer.Speak(string.Format("The current temperature is {0} degrees.",
+                                                   (int)weatherForecast.TemperatureCurrent));
+            _speechSynthesizer.Speak(string.Format("Today's low will be {0}, and the high will be {1}.",
+                                                   (int)weatherForecast.TemperatureMinimum,
+                                                   (int)weatherForecast.TemperatureMaximum));
+        }
+    }
+}
